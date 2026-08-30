603c898ddc49bfa8d88e07daf957de7e1ee3be27
thakursamsingh07-beep <thakur.samsingh.07@gmail.com>
2026-08-30 23:01:01 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='samisha-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
