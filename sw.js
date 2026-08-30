125ab14e0995c7630c3eb9e5b0e9b5685c757d49
thakursamsingh07-beep <thakur.samsingh.07@gmail.com>
2026-08-31 02:24:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='samisha-v2';
+const CACHE='samisha-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='samisha-v2').map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='samisha-v3').map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
   e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
